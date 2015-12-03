7d3818f5afa763629ff4a0866355be400cfc2cce
Jonathan Ortiz <jonathano1367@yahoo.com>
2015-12-02 20:01:15 -0800

    Home Page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	
+	
+
+  function __construct()
+    {
+        parent::__construct();
+
+
+    }
+
 	/**
 	 * Index Page for this controller.
 	 *
